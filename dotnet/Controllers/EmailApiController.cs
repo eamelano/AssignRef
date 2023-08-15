55eeef537f79c808f25bf019c3b102386c59ae71
Edgar Melano <122847644+eamelano@users.noreply.github.com>
2023-08-15 11:58:48 -0700

    Add files via upload

---
+++ dotnet/Controllers/EmailApiController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sabio.Models.Domain.Emails;
+using Sabio.Services;
+using Sabio.Services.Interfaces;
+using Sabio.Web.Controllers;
+using Sabio.Web.Models.Responses;
+using System;
+
+namespace Sabio.Web.Api.Controllers
+{
+    [Route("api/emails")]
+    [ApiController]
+    public class EmailApiController : BaseApiController
+    {
+        private IEmailService _service = null;
+        public EmailApiController(IEmailService service,
+            ILogger<EmailApiController> logger) : base(logger)
+        {
+            _service = service;
+        }
+
+        [HttpPost("sendemail")]
+        public ActionResult<SuccessResponse> SendTransacEmail(EmailInformation model)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                _service.ReceiveEmailRequest(model);
+
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+        }
+
+        [HttpPost("contact")]
+        public ActionResult<SuccessResponse> ContactAdmin(ContactUsRequest userInfo)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                _service.ContactUsRequest(userInfo);
+
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+        }
+    }
+
+}
--- dotnet/Controllers/TestInstancesApiController.cs
+++ dotnet/Controllers/TestInstancesApiController.cs
@@ -1,1 +1,225 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sabio.Models;
+using Sabio.Models.Domain.TestInstances;
+using Sabio.Models.Requests.TestInstances;
+using Sabio.Services;
+using Sabio.Services.Interfaces;
+using Sabio.Web.Controllers;
+using Sabio.Web.Models.Responses;
+using System;
 
+namespace Sabio.Web.Api.Controllers
+{
+    [Route("api/tests/results")]
+    [ApiController]
+    public class TestInstancesApiController : BaseApiController
+    {
+        private ITestInstancesService _service = null;
+        private IAuthenticationService<int> _authService = null;
+        public TestInstancesApiController(ITestInstancesService service, IAuthenticationService<int> authService, ILogger<TestInstancesApiController> logger) : base(logger)
+        {
+            _service = service;
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemResponse<int>> Add(TestInstanceAddRequest model)
+        {
+            ObjectResult result = null;
+            int userId = _authService.GetCurrentUserId();
+
+            try
+            {
+                int id = _service.Add(model, userId);
+                ItemResponse<int> response = new ItemResponse<int>() { Item = id };
+
+                result = Created201(response);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                ErrorResponse response = new ErrorResponse(ex.Message);
+
+                result = StatusCode(500, response);
+            }
+            return result;
+        }
+
+        [HttpGet("search")]
+        public ActionResult<ItemResponse<Paged<TestInstanceAnswerCount>>> Search(int pageIndex, int pageSize, string query, DateTime startDate, DateTime endDate)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                Paged<TestInstanceAnswerCount> pagedList = _service.Search(pageIndex, pageSize, query, startDate, endDate);
+                if (pagedList == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("Records Not Found");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<TestInstanceAnswerCount>>() { Item = pagedList };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message.ToString());
+            }
+            return StatusCode(code, response);
+        }
+
+        [HttpGet]
+        public ActionResult<ItemResponse<Paged<BaseTestInstance>>> SelectAll(int pageIndex, int pageSize)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                Paged<BaseTestInstance> pagedList = _service.SelectAllInstances(pageIndex, pageSize);
+                if (pagedList == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("Records Not Found");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<BaseTestInstance>>() { Item = pagedList };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message.ToString());
+            }
+            return StatusCode(code, response);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<ItemResponse<Paged<BaseTestInstance>>> SelectById(int id,int pageIndex, int pageSize)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                Paged<BaseTestInstance> pagedList = _service.SelectByTestId(id, pageIndex, pageSize);
+                if (pagedList == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("Records Not Found");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<BaseTestInstance>>() { Item = pagedList };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message.ToString());
+            }
+            return StatusCode(code, response);
+        }
+
+        [HttpGet("user")]
+        public ActionResult<ItemResponse<Paged<BaseTestInstance>>> SelectByUserId(int pageIndex, int pageSize)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            int userId = _authService.GetCurrentUserId();
+
+            try
+            {
+                Paged<BaseTestInstance> pagedList = _service.SelectByUserId(userId, pageIndex, pageSize);
+                if (pagedList == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("Records Not Found");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<BaseTestInstance>>() { Item = pagedList };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message.ToString());
+            }
+            return StatusCode(code, response);
+        }
+
+        [HttpGet("detailed/{id:int}")]
+        public ActionResult<ItemResponse<TestInstanceDetailed>> SelectByIdDetailed(int id)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                TestInstanceDetailed record = _service.SelectByInstanceIdDetailed(id);
+                response = new ItemResponse<TestInstanceDetailed>() { Item = record };
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message.ToString());
+            }
+
+            return StatusCode(code, response);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<SuccessResponse> SoftDelete(int id, int statusId)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                _service.UpdateStatus(id, statusId);
+
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+            return StatusCode(code, response);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<SuccessResponse> Update(TestInstanceUpdateRequest model, int id)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            int userId = _authService.GetCurrentUserId();
+
+            try
+            {
+                _service.Update(model, id, userId);
+
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+            return StatusCode(code, response);
+        }
+
+    }
+}
