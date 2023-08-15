ede302062de2dcdf9ea5efae22b996f0affd0519
Edgar Melano <122847644+eamelano@users.noreply.github.com>
2023-08-15 12:01:00 -0700

    Add files via upload

---
+++ dotnet/Interfaces/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using Sabio.Models.Domain.Emails;
+using sib_api_v3_sdk.Model;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        void ReceiveEmailRequest(EmailInformation model);
+        void ContactUsRequest(ContactUsRequest userInfo);
+        void SendUserAuthEmail(string token, string email, string urlExtension, EmailType emailType, int userRole = 0);
+    }
+}
---
+++ dotnet/Interfaces/ITestInstancesService.cs
@@ -0,0 +1,21 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain.TestInstances;
+using Sabio.Models.Requests.TestAnswers;
+using Sabio.Models.Requests.TestInstances;
+using System;
+using System.Collections.Generic;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface ITestInstancesService
+    {
+        int Add(TestInstanceAddRequest model, int userId);
+        Paged<BaseTestInstance> SelectAllInstances(int pageIndex, int pageSize);
+        Paged<BaseTestInstance> SelectByTestId(int id, int pageIndex, int pageSize);
+        TestInstanceDetailed SelectByInstanceIdDetailed(int id);
+        Paged<BaseTestInstance> SelectByUserId(int userId, int pageIndex, int pageSize);
+        void UpdateStatus(int id, int statusId);
+        void Update(TestInstanceUpdateRequest model, int id, int userId);
+        Paged<TestInstanceAnswerCount> Search(int pageIndex, int pageSize, string query, DateTime startDate, DateTime endDate);
+    }
+}
