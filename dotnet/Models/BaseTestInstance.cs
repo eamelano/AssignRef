0df78ee2e99507c2416bf05fcf397cacb82b9b42
Edgar Melano <122847644+eamelano@users.noreply.github.com>
2023-08-15 11:52:41 -0700

    Create BaseTestInstance.cs

---
+++ dotnet/Models/BaseTestInstance.cs
@@ -0,0 +1,9 @@
+    public class BaseTestInstance
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public LookUp Status { get; set; }
+        public BaseUser User { get; set; }
+        public LookUp Test { get; set; }
+        public LookUp TestType { get; set; }
+    }
