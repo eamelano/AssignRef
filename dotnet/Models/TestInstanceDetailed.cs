627e7d89cd3a3e48e047e39949503bcc99bbd660
Edgar Melano <122847644+eamelano@users.noreply.github.com>
2023-08-29 09:04:50 -0700

    Update TestInstanceDetailed.cs

--- dotnet/Models/TestInstanceDetailed.cs
+++ dotnet/Models/TestInstanceDetailed.cs
@@ -7,5 +7,4 @@ public class TestInstanceDetailed
         public LookUp Status { get; set; }
         public TestV1 Test { get; set; }
         public List<TestQuestionDetailed> Questions { get; set; }
-
     }
