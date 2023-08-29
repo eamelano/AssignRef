fb86d4547228caabed9bd78dc2ff50b43de572bc
Edgar Melano <122847644+eamelano@users.noreply.github.com>
2023-08-29 11:03:33 -0700

    Update dbo.AnswerOptions_Batch.UserDefinedTableType.sql

--- sql/dbo.AnswerOptions_Batch.UserDefinedTableType.sql
+++ sql/dbo.AnswerOptions_Batch.UserDefinedTableType.sql
@@ -1,8 +1,10 @@
 GO
 
-CREATE TYPE [dbo].[AnswerOptions_Batch] AS TABLE(
-	[Text] [nvarchar](500) NULL,
-	[Value] [nvarchar](100) NULL,
-	[AdditionalInfo] [nvarchar](200) NULL
-)
+CREATE 
+	TYPE [dbo].[AnswerOptions_Batch] AS TABLE
+		(
+		[Text] [nvarchar](500) NULL,
+		[Value] [nvarchar](100) NULL,
+		[AdditionalInfo] [nvarchar](200) NULL
+		)
 GO
